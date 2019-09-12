4be4253137716a764021c815aabe8e18de5ff0c2
maozongxing <maozx@dayiai.com>
2019-09-12 16:24:57 +0800

    Initial commit

---
+++ src/main/java/com/mao/KeepYoungerAdminApplication.java
@@ -0,0 +1,13 @@
+package com.mao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepYoungerAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepYoungerAdminApplication.class, args);
+	}
+
+}
